173eb9b5d85982d860da6fd158eb1eeb57ed5a4b
reubinoff <reubinoff@gmail.com>
2020-01-02 11:20:37 +0200

    sdsd

--- start_test.py
+++ start_test.py
@@ -1,12 +1,12 @@
 import requests
 import cloudshell.api.cloudshell_api as cs_api
 
-IP = "52.177.135.165"
-blueprint = "shared device11"
-session = cs_api.CloudShellAPISession(IP, 'admin', 'admin', 'Global')
-resourceDict = {}
-res = session.CreateImmediateReservation(blueprint, "admin",5)
-print(res)
+# IP = "52.177.135.165"
+# blueprint = "shared device11"
+# session = cs_api.CloudShellAPISession(IP, 'admin', 'admin', 'Global')
+# resourceDict = {}
+# res = session.CreateImmediateReservation(blueprint, "admin",5)
+# print(res)
 
 
 
