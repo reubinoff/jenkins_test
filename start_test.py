67f35498797060f9bf76c51a4d1b84f93016df34
reubinoff <reubinoff@gmail.com>
2020-01-02 11:01:50 +0200

    sd

--- start_test.py
+++ start_test.py
@@ -1,4 +1,13 @@
 import requests
+import cloudshell.api.cloudshell_api as cs_api
+
+IP = "52.177.135.165"
+blueprint = "shared device11"
+session = cs_api.CloudShellAPISession(IP, 'admin', 'admin', 'Global')
+resourceDict = {}
+res = session.CreateImmediateReservation(blueprint, "admin",5)
+print(res)
+
 
 
 url = "https://verison-robot.azurewebsites.net/api/perfecto?code=x5u5GaCpIgGsw34YIYoo5HL/oWA6sIgTnReZKsurUdJisN3WXn3SDA=="
